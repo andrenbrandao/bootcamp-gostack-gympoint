c3c7d5453760d561a8f9d387e759c01615c3f6ca
André Brandão <andrenbrandao@gmail.com>
2019-10-18 10:21:29 -0300

    Add Student create

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,43 @@
+import * as Yup from 'yup';
+import Student from '../models/Student';
+
+class StudentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number()
+        .required()
+        .positive()
+        .integer(),
+      weight: Yup.number()
+        .required()
+        .positive()
+        .truncate(),
+      height: Yup.number()
+        .required()
+        .positive()
+        .integer(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const { email } = req.body;
+
+    const studentExists = await Student.findOne({ where: { email } });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'Student already exists' });
+    }
+
+    const student = await Student.create(req.body);
+
+    return res.json(student);
+  }
+}
+
+export default new StudentController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
 
 const router = new Router();
 
 router.post('/sessions', SessionController.store);
+router.post('/students', StudentController.store);
 
 export default router;
