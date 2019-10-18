630d8889598a511f19e5e217226efb794eb16e33
André Brandão <andrenbrandao@gmail.com>
2019-10-18 11:19:00 -0300

    Add user authentication middleware
    - Students can only be managed by authenticated users

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,13 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const router = new Router();
 
 router.post('/sessions', SessionController.store);
+
+router.use(authMiddleware);
 router.get('/students', StudentController.index);
 router.get('/students/:id', StudentController.show);
 router.post('/students', StudentController.store);
