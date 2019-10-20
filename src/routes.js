1901f159adb06e634a7162e3e35353acfb304212
André Brandão <andrenbrandao@gmail.com>
2019-10-20 12:08:27 -0300

    Create subscription Plans

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
+import PlanController from './app/controllers/PlanController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -14,5 +15,11 @@ router.get('/students', StudentController.index);
 router.get('/students/:id', StudentController.show);
 router.post('/students', StudentController.store);
 router.put('/students/:id', StudentController.update);
+
+router.get('/plans', PlanController.index);
+router.get('/plans/:id', PlanController.show);
+router.post('/plans', PlanController.store);
+router.put('/plans/:id', PlanController.update);
+router.delete('/plans/:id', PlanController.delete);
 
 export default router;
