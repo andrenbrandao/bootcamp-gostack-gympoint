9905e7d6b2eac3511bcf9f13c488cde76c2b5122
André Brandão <andrenbrandao@gmail.com>
2019-12-10 16:36:38 -0300

    Add private routes configuration

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
+import StudentList from '../pages/StudentList';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
+      <Route path="/students" component={StudentList} isPrivate />
     </Switch>
   );
 }
