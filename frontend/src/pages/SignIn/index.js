998aed8736b60f3135a8744e1424507495fc3653
André Brandão <andrenbrandao@gmail.com>
2019-12-10 18:02:52 -0300

    Add style to SignIn page

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
+import verticalLogo from '~/assets/vertical-logo.svg';
+
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+      <img src={verticalLogo} alt="Gympoint" />
+
+      <form>
+        <label htmlFor="email">
+          SEU E-MAIL
+          <input type="email" placeholder="exemplo@email.com" />
+        </label>
+
+        <label htmlFor="password">
+          SUA SENHA
+          <input type="password" placeholder="**************" />
+        </label>
+
+        <button type="submit">Entrar no sistema</button>
+      </form>
+    </>
+  );
 }
