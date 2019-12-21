7ae5fdc1d250a99ce5ab36fc50fe2d1e82f7194c
André Brandão <andrenbrandao@gmail.com>
2019-12-21 14:46:53 -0300

    Add react navigation to react native

---
+++ mobile/src/routes.js
@@ -0,0 +1,10 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import SignIn from '~/pages/SignIn';
+
+export default createAppContainer(
+  createStackNavigator({
+    SignIn,
+  }),
+);
