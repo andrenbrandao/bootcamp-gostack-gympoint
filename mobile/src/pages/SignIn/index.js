a6d59ed872ecb8d43f361588b8993e0826fdb5c6
André Brandão <andrenbrandao@gmail.com>
2019-12-22 14:39:51 -0300

    mobile: Add redux and SignIn

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Container, Form, FormInput, SubmitButton } from './styles';
 
 import logo from '~/assets/logo.png';
+import { signInRequest } from '~/store/modules/auth/actions';
 
 export default function SignIn() {
+  const dispatch = useDispatch();
+
+  const [id, setId] = useState('');
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit() {
+    dispatch(signInRequest(id));
+  }
+
   return (
     <Container>
       <Image source={logo} />
@@ -16,8 +27,12 @@ export default function SignIn() {
           autoCapitalize="none"
           keyboardType="numeric"
           placeholder="Informe seu ID de cadastro"
+          value={id}
+          onChangeText={setId}
         />
-        <SubmitButton onPress={() => {}}>Entrar no sistema</SubmitButton>
+        <SubmitButton loading={loading} onPress={handleSubmit}>
+          Entrar no sistema
+        </SubmitButton>
       </Form>
     </Container>
   );
---
+++ mobile/src/store/modules/auth/sagas.js
@@ -0,0 +1,47 @@
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+
+import { Alert } from 'react-native';
+// import history from '~/services/history';
+import api from '~/services/api';
+
+import { signInSuccess, signFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { id } = payload;
+
+    const response = yield call(api.get, `students/${id}`);
+
+    const user = response.data;
+
+    yield put(signInSuccess(user));
+
+    // history.push('/students');
+  } catch (err) {
+    Alert.alert(
+      'Falha na autenticação',
+      'Houve um erro na autenticação, verifique seus dados.',
+    );
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export function signOut() {
+  history.push('/');
+}
+
+export default all([
+  // takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_OUT', signOut),
+]);
